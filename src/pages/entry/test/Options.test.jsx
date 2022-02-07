b084bfa578c46b37927f741ad7d6faf538787773
james <james@quizlicious.com>
2022-02-06 21:34:41 -0500

    Changes for lesson 54

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -13,3 +13,19 @@ test("displays image for each scoop option from server", async () => {
   const altTest = scoopImages.map((element) => element.alt);
   expect(altTest).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("Displays images for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const altTest = toppingImages.map((element) => element.alt);
+  expect(altTest).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
