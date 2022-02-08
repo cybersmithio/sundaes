90f46f28286d25c220807e7d6c017fd91188acff
james <james@quizlicious.com>
2022-02-07 20:32:50 -0500

    Done lesson 71, all tests passing

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,6 @@
 import { Form, Col} from "react-bootstrap";
 
-export default function ToppingOption({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath, updateItemCount }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -8,9 +8,14 @@ export default function ToppingOption({ name, imagePath }) {
         style={{ width: "75%" }}
         src={`http://localhost:3030/${imagePath}`}
       />
-      <Form.Group>
-        <Form.Label>{name}</Form.Label>
-        <Form.Check/>
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check
+          type="checkbox"
+          label={name}
+          onChange={(e) => {
+            updateItemCount(name, e.target.checked ? 1 : 0);
+          }}
+        />
       </Form.Group>
     </Col>
   );
